ff19627d44c18b1384391ee3960c76ec8184f5c6
yoshi <32868028+yoshi1125hisa@users.noreply.github.com>
2018-11-05 10:23:08 +0900

    [Update] プロジェクトファイル一式を追加

---
+++ app/app.module.ts
@@ -0,0 +1,31 @@
+import { NgModule, ErrorHandler } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+import { MyApp } from './app.component';
+import { HelloWorldPage } from '../pages/hello-world/hello-world';
+
+
+import { StatusBar } from '@ionic-native/status-bar';
+import { SplashScreen } from '@ionic-native/splash-screen';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    HelloWorldPage
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HelloWorldPage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule {}
---
+++ pages/hello-world/hello-world.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-hello-world',
+  templateUrl: 'hello-world.html'
+})
+export class HelloWorldPage {
+
+  constructor(public navCtrl: NavController) {
+  }
+  
+}
